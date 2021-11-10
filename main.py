7271a69b9294af562753d1981ee2c5e0707492bd
Yanyun Wu <z5191449@ad.unsw.edu.au>
2021-11-10 12:11:29 +0800

    save images

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import random
 from math import sqrt
 import pyefd
 
-SEQUENCE = '01'
+SEQUENCE = '02'
 # If the cells' movement between two frame is less than DIST, it's the same cell.
 DIST = 21
 # The kernel size in opening.
